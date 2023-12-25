6c61a20538465b1886563df5ae052d2affdd7309
Gaurav <rajkumargaurav2112@gmail.com>
2023-12-25 18:45:56 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -11,19 +11,5 @@ app.listen(3000,async ()=>{
 
     await connect();
     console.log("Mongo db connected.");
-    // const tweet = await Tweet.create({
-    //     content:'Third tweet'
-       
-    // })
-
-    // const tweet = await Tweet.find({userEmail:'a@b.com'});
-   const tweetRepo = new TweetRepository();
-
-   const tweet = await tweetRepo.create({content:"twit with hooks"});
-   console.log(tweet);
-
-//    const tweet= await tweetRepo.getAll(2,4);
-   
-//    console.log(tweet[0].contentWithEmail);
-   
+    
 });    
